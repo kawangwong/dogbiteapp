a840005f51f2f8795655635be03121afd0985386
kawangwong <nycazn724@gmail.com>
2021-09-09 14:08:07 -0400

    Update application.py

--- application.py
+++ application.py
@@ -6,8 +6,8 @@ application = app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World here is a basic site that lets you look at the data of dog bites related and where they occurred in NYC \n' \
-           'Here you can change the top URL to "/doggender for the number of dog bites based on gender or /borough for borough location'
+    return 'Hello World here is a basic site that lets you look at the data of dog bites related and where they occurred in NYC. \n' \
+           'Here you can change the top URL to "/hello/"yourname" to have a greeting with your name, /gender/gender for the number of dog bites based on gender or /borough for borough location'
 
 
 @app.route('/hello/<name>')
